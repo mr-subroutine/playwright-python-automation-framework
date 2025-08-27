b4eba25571a176e7f96c85678db61174b5f2bb71
Darryl <62851935+mr-subroutine@users.noreply.github.com>
2025-08-27 09:48:07 -0700

    Added test for character name required.  Fails if no characters are entered, spaces are also removed.

--- milestone1_pytest_basics/player.py
+++ milestone1_pytest_basics/player.py
@@ -1,7 +1,11 @@
 
 
-def name_character(name: str):
+def player_name_count(name: str):
     count = 0
     for char in name:
         count += 1
     return count
+
+def return_player_name_for_input_check(name: str):
+    name = name.strip()
+    return name
--- milestone1_pytest_basics/tests/test_player_name.py
+++ milestone1_pytest_basics/tests/test_player_name.py
@@ -1,6 +1,17 @@
-from milestone1_pytest_basics.player import name_character
+from milestone1_pytest_basics.player import player_name_count, return_player_name_for_input_check
 
-def test_player_name():
-    count = name_character("Tester18")  # change name_character argument to test names, 8 character max
+
+def test_player_name_max_characters():
+    count = player_name_count("Tester7")  # change name_character argument to test names, 8 character max
     print(f"The count is: {count}")
     assert count <= 8
+
+def test_player_name_required_characters():
+    name_count = 0
+    name = return_player_name_for_input_check("One")  # change name_character argument must have name, fails when no named entered.
+
+    for char in name:
+        name_count += 1
+        print(f"Required Test: The count is: {name_count}")
+
+    assert name_count > 0
