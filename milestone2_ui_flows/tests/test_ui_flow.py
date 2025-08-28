e37defec2cb456964d0599774d7c0ccf17023abe
Darryl <62851935+mr-subroutine@users.noreply.github.com>
2025-08-28 15:25:22 -0700

    finished test for logging in with correct pw

--- milestone2_ui_flows/tests/test_ui_flow.py
+++ milestone2_ui_flows/tests/test_ui_flow.py
@@ -5,9 +5,11 @@ def test_logging_in(page: Page):
     page.goto("https://www.saucedemo.com/")  # Test webpage not created by this project owner
     page.locator("#user-name").fill("standard_user")
     page.locator("#password").fill("secret_sauce")
-    page.get_by_role("button").click()
 
     #WIP ADD EXPECT FOR PROPER LOGIN
+    with page.expect_navigation():
+        page.get_by_role("button").click()
+        expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
 
 
 
